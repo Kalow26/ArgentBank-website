adbfa62686547c2534a555848ea1483d1330ebdf
Laurent De La Calle <laurentdelacalle@hotmail.com>
2023-12-12 18:07:44 +0100

    implantation de la fonction fetch avec stockage du token et gestion des erreurs

--- Frontend/src/app/store/features/userSlice.js
+++ Frontend/src/app/store/features/userSlice.js
@@ -1,17 +1,41 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const url = "http://localhost:3001/api/v1/user/";
+
+export const fetchUserByEmail = createAsyncThunk(
+  "users/fetchByEmailStatus",
+  async (userInfo, thunkAPI) => {
+    try {
+      const response = await fetch(`${url}login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json;charset=utf-8" },
+        body: JSON.stringify(userInfo),
+      });
+      if (!response.ok) {
+        const errorMessage = await response.json();
+        return thunkAPI.rejectWithValue(errorMessage);
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue({ message: "Network error" });
+    }
+  }
+);
 
 const initialState = {
   userInfo: {},
   statusMessage: "",
+  isLogged: false,
+  rememberMe: false,
 };
 
 export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    login: (state, action) => {
-      /* type: "/user/login", payload : "email, password" */
-    },
+    login: (state, action) => {},
     getUserProfile: (state, action) => {
       /* type: "/user/getUserProfile", payload : "token" */
     },
@@ -21,6 +45,22 @@ export const userSlice = createSlice({
     logout: (state, action) => {
       /* type : "LOGOUT", payload : "token" */
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchUserByEmail.pending, (state) => {
+      state.statusMessage = "Logging in...";
+    });
+    builder.addCase(fetchUserByEmail.fulfilled, (state, action) => {
+      const storage = action.meta.arg.rememberMe
+        ? localStorage
+        : sessionStorage;
+      storage.setItem("token", action.payload.body.token);
+      state.isLogged = true;
+      state.statusMessage = action.payload.message;
+    });
+    builder.addCase(fetchUserByEmail.rejected, (state, action) => {
+      state.statusMessage = action.payload.message;
+    });
   },
 });
 
--- Frontend/src/pages/login/Login.jsx
+++ Frontend/src/pages/login/Login.jsx
@@ -1,26 +1,30 @@
 import { useState } from "react";
 import "./login.css";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUserByEmail } from "../../app/store/features/userSlice";
 
 const Login = () => {
-  const [userInfo, setUserInfo] = useState({ email: "", password: "" });
+  const [userInfo, setUserInfo] = useState({
+    email: "",
+    password: "",
+    rememberMe: false,
+  });
+  console.log(userInfo.rememberMe);
+  const statusMessage = useSelector((state) => state.user.statusMessage);
 
   const dispatch = useDispatch();
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    dispatch(fetchUserByEmail(userInfo));
+  };
 
   return (
     <section className="main bg-dark">
       <div className="sign-in-content">
         <i className="fa fa-user-circle sign-in-icon"></i>
         <h1>Sign In</h1>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            dispatch({
-              type: "user/login",
-              payload: userInfo,
-            });
-          }}
-        >
+        <form onSubmit={submitForm}>
           <div className="input-wrapper">
             <label htmlFor="username">Username</label>
             <input
@@ -42,12 +46,19 @@ const Login = () => {
             />
           </div>
           <div className="input-remember">
-            <input type="checkbox" id="remember-me" />
+            <input
+              type="checkbox"
+              id="remember-me"
+              onChange={() =>
+                setUserInfo({ ...userInfo, rememberMe: !userInfo.rememberMe })
+              }
+            />
             <label htmlFor="remember-me">Remember me</label>
           </div>
           <button type="submit" className="sign-in-button">
             Sign In
           </button>
+          {statusMessage}
         </form>
       </div>
     </section>
